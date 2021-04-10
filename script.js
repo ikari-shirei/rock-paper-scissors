01691ef8eb1fba63f2c6b9877131863ecdc665ec
ikari-shirei <algrsyu@protonmail.com>
2021-04-10 19:25:00 +0300

    Game playable but only 5 times, there is chance of tie

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ function computerPlay() {
 
 function play(playerSelection, computerSelection = computerPlay()) {
   // Make player selection lower case
-  playerSelection = playerSelection.toLowerCase();
+  if (playerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+  } else {
+    //if input doesn't exist, default selection is rock
+    playerSelection = 'rock';
+  }
   // Make first letter of player selection uppercase, others lowercase.
   let suitablePlayerSelection =
     playerSelection[0].toUpperCase() + playerSelection.slice(1);
-  // Make first letter of player selection uppercase, others lowercase.
+  // Make first letter of computer selection uppercase, others lowercase.
   let suitableComputerSelection =
     computerSelection[0].toUpperCase() + computerSelection.slice(1);
   // If player selection not one of the elements, throw a error.
@@ -35,3 +40,43 @@ function play(playerSelection, computerSelection = computerPlay()) {
     return `You win. ${suitablePlayerSelection} beats ${computerSelection}.`;
   }
 }
+
+function game() {
+  //count win, lose and tie
+  let win = 0;
+  let lose = 0;
+  let tie = 0;
+  //save results into an array
+  let results = [];
+  //iterator is in function scope for later use
+  let i = undefined;
+  console.log(results);
+  //take 5 inputs, compare them with using play function and save into results array
+  for (i = 1; i <= 5; i++) {
+    results.push(
+      play(prompt(`Rock, paper or scissor? (Default choice is rock.)`))
+    );
+  }
+
+  //iterate input data, count win, lose and tie
+  for (let j = 0; j < results.length; j++) {
+    if (results[j].includes('You win.')) {
+      win++;
+    } else if (results[j].includes('You lose.')) {
+      lose++;
+    } else {
+      tie++;
+    }
+  }
+
+  //compare win and loses, find if user won or lost
+  if (win > lose) {
+    return `You win ${win} times, lose ${lose} times and tie ${tie} times. You won, computer lost.`;
+  } else if (lose > win) {
+    return `You win ${win} times, lose ${lose} times and tie ${tie} times. You lost, computer won.`;
+  } else {
+    return `You win ${win} times, lose ${lose} times and tie ${tie} times. Nobody won. It's tie.`;
+  }
+}
+
+console.log(game());
