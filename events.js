adf18cc5d9ab9ec0a2df59f95c0929673c32cfc3
ikari-shirei <algrsyu@protonmail.com>
2021-04-19 22:56:37 +0300

    Events script

---
+++ events.js
@@ -0,0 +1,6 @@
+let rock = document.querySelector('#rock');
+let paper = document.querySelector('#paper');
+let scissors = document.querySelector('#scissors');
+
+let winDom = document.querySelector('#win');
+let loseDom = document.querySelector('#lose');
